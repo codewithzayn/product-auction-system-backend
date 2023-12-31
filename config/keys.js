549a433747aaf4293b9cd8cfb197bae4fefad529
zainabbas <zainabbaskhakhi123@gmail.com>
2023-12-31 14:16:20 +0500

    first commit

--- config/keys.js
+++ config/keys.js
@@ -1,14 +1,13 @@
 const mongoose = require("mongoose");
 
-const db = process.env.MONGO_URI;
-// console.log("db",db)
+const db = process.env.ONLINE_MONGO_URI;
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("MongoDB is connected");
+    console.log("MongoDB is connected",db);
   } catch (err) {
     console.error(err.message);
     process.exit(1);
